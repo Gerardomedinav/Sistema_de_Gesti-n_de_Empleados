380fbe11ec1ad97b0c01165df0d67bdf37f835b0
Tu Nombre <tuemail@ejemplo.com>
2025-09-11 12:19:21 -0300

    modifica el vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/sass/app.scss", "resources/js/app.js"],
+            input: ['resources/sass/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
     build: {
         manifest: true,
-        outDir: "public/build",
-        rollupOptions: { input: "resources/js/app.js" },
+        outDir: 'public/build',
+        rollupOptions: {
+            input: 'resources/js/app.js', // asegúrate de que el archivo sea correcto
+        },
     },
+    base: '/', // Asegúrate de que esto sea correcto para tu entorno
 });
