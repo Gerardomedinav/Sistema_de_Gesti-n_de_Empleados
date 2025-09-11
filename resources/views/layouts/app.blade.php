ffe40c07909ecc0f0e0206e6a25bb8afea5c9ca9
Tu Nombre <tuemail@ejemplo.com>
2025-09-11 16:14:58 -0300

    asset() funcion en app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">    
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -14,9 +14,9 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles --> 
-         {{--   @vite(['resources/sass/app.scss', 'resources/js/app.js'])--}}
-         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-          
+       <link rel="stylesheet" href="{{ Vite::asset('resources/sass/app.scss') }}">
+       <script src="{{ Vite::asset('resources/js/app.js') }}"></script>
+
   </head>
 <body>
     <div id="app">
