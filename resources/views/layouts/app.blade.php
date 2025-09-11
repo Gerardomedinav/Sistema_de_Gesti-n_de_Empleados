21247cdfe9ad8266cee118ac34876a471804ebb1
Tu Nombre <tuemail@ejemplo.com>
2025-09-11 15:24:07 -0300

    modifica el vite.config.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,10 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
-          @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <!-- Styles --> 
+         {{--   @vite(['resources/sass/app.scss', 'resources/js/app.js'])--}}
+         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+          
   </head>
 <body>
     <div id="app">
